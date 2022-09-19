588d111ba3c985f7d9c8c790efe4e3661dcc3ff3
kristina.sigal <kulrikh@gmail.com>
2022-09-19 22:43:17 +0300

    Add last version

--- Program.cs
+++ Program.cs
@@ -1,1 +1,40 @@
 ﻿
+string[] StringArrayRead(char ch)
+{
+    Console.WriteLine($"Введите массив строк, разделяя строки символом \"{ch}\"");
+    string result = Console.ReadLine();
+    return result.Split(ch);
+}
+
+string[] FindElem(string[] arr)
+{
+    int count = 0;
+    foreach (string item in arr)
+    {
+        if (item.Length < 4) count++;
+    }
+    if (count == 0) return new string[] {"Нет данных"};
+
+    string[] result = new string[count];
+
+    int resultIndex = 0;
+    foreach (string item in arr)
+    {
+        if (item.Length < 4)
+        {
+            result[resultIndex] = item;
+            resultIndex++;
+        }
+    }
+    return result;
+}
+
+void PrintArray(string[] arr)
+{
+    foreach (string item in arr)
+    {
+        Console.Write($"{item} ");
+    }
+}
+
+PrintArray(FindElem(StringArrayRead(';')));
